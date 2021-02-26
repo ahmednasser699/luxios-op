0e7cc4e9b8a7f7c05285543120f54e7e02f5bf37
ahmednasser <ahmednasser.ce@gmail.com>
2021-02-26 06:06:57 +0200

    task

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,27 @@
-
-
 $("body").niceScroll({
-        cursorcolor:"red",
-        cursorwidth: "8px"
+        cursorcolor:"#ffc107",
+        cursorwidth: "8px",   
+});
+
+const houses = document.querySelector('.houses');
+const scrollLine = document.getElementById('scroll');
+const scrollbtn = document.querySelector('.scrollbtn');
+
+scrollLine.addEventListener("click", function () {
+    houses.scrollIntoView({ behavior: "smooth" })
+});
+
+scrollbtn.addEventListener('click', function () {
+ window.scroll({top: 0, left: 0, behavior: 'smooth'});
+});
+
+window.onscroll = function (e) {
+    if (window.pageYOffset > 1000) {
+        scrollbtn.style.zIndex='99';
         
-    });
+        scrollbtn.style.opacity = '1';
+    } else {
+       scrollbtn.style.zIndex='-1';
+        scrollbtn.style.opacity = '0';
+   }
+}
